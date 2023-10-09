e2caa75a78bc5e78ac05b79ef6e1ee39ef79994c
AbdulrahmanRezk <108305550+abdelrahmanrizk00@users.noreply.github.com>
2023-10-09 22:34:59 +0300

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,27 +1,27 @@
-correct_Number = 10
-Try = 5
+import random
+Try = 10
 try:
     print("^^That is an Expectation Game^^".center(80, "#"))
     print("Guess Correct Number To Win".center(80, "#"))
+    correct_Number = random.randint(10, 100)
     user_in = int(input("Enter Your Expected Number => "))
     while user_in != correct_Number:
 
         if user_in > correct_Number:
-            print("-Your expectation is Higher than expect #Expect less#")
-            if user_in in range(11, 16):
-                print("#You are close to the correct predication#")
-
-        elif user_in in range(5, 10):
-            print("-Your expectation is lower than expect\n#You are close to the correct predication#")
-
-        else:
-            print("-your expectations are lower* #Expect more#")
+            print("-Your expectation is Higher than correct ")
+            if user_in in range(correct_Number, (correct_Number + 5)):
+                print("#You are close to the correct predication#...Expect less")
+        if user_in < correct_Number:
+            print("-Your expectation is lower than correct")
+        if user_in < correct_Number and user_in in range(correct_Number, (correct_Number - 5)):
+            print("#You are close to the correct predication#...Expect more")
 
         Try -= 1
         print(f"*You have {Try} chances.")
         if Try == 1:
             print("**You have last chance ||Be Careful||")
         elif Try == 0:
+            print(f"number was ==>{correct_Number}")
             break
 
         user_in = int(input("Enter Your Expected Number again => "))
